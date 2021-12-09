bd5f16d237e896a81e037fb4ea9b054ad35bebb6
kojix2 <2xijok@gmail.com>
2021-12-09 13:59:59 +0900

    Fix dir structure

---
+++ ext/twobit/twobit.c
@@ -0,0 +1,33 @@
+#include "twobit.h"
+
+VALUE cTwoBit;
+
+static VALUE two_bit_open(VALUE self){
+}
+
+static VALUE two_bit_close(VALUE self){
+}
+
+static VALUE two_bit_chroms(VALUE self) {
+}
+
+static VALUE two_bit_info(VALUE self) {
+}
+
+static VALUE two_bit_sequence(VALUE self) {
+}
+
+static VALUE two_bit_bases(VALUE self) {
+}
+
+void
+Init_twoBit(void)
+{
+  cTwoBit = rb_define_class("Twobit", rb_cObject);
+  rb_define_singleton_method(cTwoBit, "open", two_bit_open, 0);
+  rb_define_method(cTwoBit, "close", two_bit_close, 0);
+  rb_define_method(cTwoBit, "chroms", two_bit_chroms, 0);
+  rb_define_method(cTwoBit, "info", two_bit_info, 0);
+  rb_define_method(cTwoBit, "sequence", two_bit_sequence, 0);
+  rb_define_method(cTwoBit, "bases", two_bit_bases, 0);
+}
---
+++ ext/twobit/twobit.h
@@ -0,0 +1,7 @@
+#ifndef TWOBIT_H
+#define TWOBIT_H 1
+
+#include "ruby.h"
+#include "2bit.h"
+
+#endif /* TWOBIT_H */
