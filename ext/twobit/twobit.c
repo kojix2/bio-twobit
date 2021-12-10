359a8bb1ec0b3e8984ac9190de96735a793d2920
kojix2 <2xijok@gmail.com>
2021-12-10 09:47:19 +0900

    Format

--- ext/twobit/twobit.c
+++ ext/twobit/twobit.c
@@ -27,6 +27,7 @@ static size_t
 TwoBit_memsize(const void *ptr)
 {
   const TwoBit *data = ptr;
+
   return data ? sizeof(*data) : 0;
 }
 
@@ -34,6 +35,7 @@ static TwoBit *getTwoBit(VALUE self)
 {
   TwoBit *ptr = NULL;
   TypedData_Get_Struct(self, TwoBit, &Twobit_type, ptr);
+
   return ptr;
 }
 
@@ -44,40 +46,62 @@ twobit_init(VALUE klass)
 }
 
 static VALUE
-twobit_new(VALUE klass, VALUE fname, VALUE storeMasked)
+twobit_new(VALUE klass, VALUE fpath, VALUE storeMasked)
 {
-  char *fnamec = StringValueCStr(fname);
-  int store_maskedc = NUM2INT(storeMasked);
-  TwoBit *tb = twobitOpen(fnamec, store_maskedc);
+  char *path;
+  int mask;
+
+  path = StringValueCStr(fpath);
+  mask = NUM2INT(storeMasked);
+
+  TwoBit *tb = twobitOpen(path, mask);
+
   return TypedData_Wrap_Struct(klass, &Twobit_type, tb);
 }
 
 static VALUE
 twobit_nchroms(VALUE self)
 {
-  TwoBit *tb = getTwoBit(self);
-  uint32_t nc = tb->hdr->nChroms;
-  VALUE val = UINT2NUM(nc);
+  TwoBit *tb;
+  VALUE val;
+  int32_t nc;
+
+  tb = getTwoBit(self);
+  nc = tb->hdr->nChroms;
+
+  val = UINT2NUM(nc);
+
   return val;
 }
 
 static VALUE
 twobit_file_size(VALUE self)
 {
-  TwoBit *tb = getTwoBit(self);
-  uint64_t file_size = tb->sz;
-  VALUE val = ULL2NUM(file_size);
+  TwoBit *tb;
+  uint64_t size;
+  VALUE val;
+
+  tb = getTwoBit(self);
+  size = tb->sz;
+  val = ULL2NUM(size);
+
   return val;
 }
 
 static VALUE
 twobit_sequence(VALUE self, VALUE chrom, VALUE start, VALUE end)
 {
-  char *c = StringValueCStr(chrom);
-  uint32_t s = NUM2ULONG(start);
-  uint32_t e = NUM2ULONG(end);
-  TwoBit *tb = getTwoBit(self);
-  char *str = twobitSequence(tb, c, s, e);
+  char *ch, *str;
+  uint32_t st, en;
+  TwoBit *tb;
+
+  ch = StringValueCStr(chrom);
+  st = NUM2ULONG(start);
+  en = NUM2ULONG(end);
+  tb = getTwoBit(self);
+
+  str = twobitSequence(tb, ch, st, en);
+
   return rb_str_new2(str);
 }
 
@@ -90,7 +114,9 @@ twobit_bases(VALUE self)
 void Init_twobit(void)
 {
   rb_Twobit = rb_define_class("Twobit", rb_cObject);
+
   rb_define_singleton_method(rb_Twobit, "new", twobit_new, 2);
+
   rb_define_method(rb_Twobit, "initialize", twobit_init, 0);
   rb_define_method(rb_Twobit, "nchroms", twobit_nchroms, 0);
   rb_define_method(rb_Twobit, "file_size", twobit_file_size, 0);
